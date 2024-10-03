bce6eec391c7c5d12350fcc6fd767a05cdf15884
funa <orz.home35@gmail.com>
2024-10-03 15:55:55 +0900

    🚧 スライドショーをSwiperに変更

--- main.js
+++ main.js
@@ -57,29 +57,22 @@ window.onclick = function (event) {
 
 
 //スライダー---------------------------------------------------------------------------------------------------------------------------
-$('.slider-inner').slick({
-	autoplay: true,//自動的に動き出すか。初期値はfalse。
-	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-	slidesToShow: 1,//スライドを画面に3枚見せる
-	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	dots: true,//下部ドットナビゲーションの表示
-	responsive: [
-		{
-		breakpoint: 900,//モニターの横幅が769px以下の見せ方
-		settings: {
-			slidesToShow: 1,//スライドを画面に2枚見せる
-			slidesToScroll: 1,//1回のスクロールで2枚の写真を移動して見せる
-		}
-	},
-	{
-		breakpoint: 650,//モニターの横幅が426px以下の見せ方
-		settings: {
-			slidesToShow: 1,//スライドを画面に1枚見せる
-			slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-		}
-	}
-]
-});
+const swiper = new Swiper('.swiper',{
+	loop: true,                         //ループ
+	centeredSlides: true,
 
+	autoplay: {                         //自動再生
+		delay: 5000,
+		disableOnInteraction:false,	 // ユーザーが操作しても自動再生を止めない
+	},
+
+	pagination: {                       //ページネーション（ドット）
+		el: '.swiper-pagination',
+		clickable: true,			// クリックによるスライド切り替えを有効にする
+	},
+
+	navigation: {                       //ナビゲーション（矢印）
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+})
